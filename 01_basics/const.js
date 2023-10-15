7840aa5b6b999e8ac570740133c43afc6601d66f
Saugat Ghimire <68614843+Saugatify@users.noreply.github.com>
2023-10-15 05:17:58 +0000

    datatype

---
+++ 01_basics/const.js
@@ -0,0 +1,18 @@
+const accountId =144553
+let accountEmail ="saugatg@gmail.com"
+var accountPassword ="1234"
+accountCity ="Kathmandu"
+let accountState;
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+//accountId =2
+accountEmail="s@gmail.com"
+accountPassword="21212"
+accountCity ="Sarlahi"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+ 
+console.log(accountId);
