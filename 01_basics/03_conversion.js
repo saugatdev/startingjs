684a73f9faa1cd0f55a89d6935a0e5f6451e1a22
Saugat Ghimire <68614843+Saugatify@users.noreply.github.com>
2023-10-15 12:47:39 +0000

    operators

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,23 +1,61 @@
-let score =null
-console.log(typeof score)
-console.log(typeof(score))
+//let score =null
+// console.log(typeof score)
+//console.log(typeof(score))
 
 //let valueInNumber = Number(score)
 
 
 
-//"33" => 33
+// "33" => 33
 //"33abc" => NaN
 //true -> 1; false - 0
 
-let isLoggedIn =1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let isLoggedIn =1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 
 
-let Number =33
-let stringNumber =String(Number);
-console.log(typeof(Number))
-console.log(stringNumber);
-console.log(typeof(stringNumber))
+// let Number =33
+// let stringNumber =String(Number);
+// console.log(typeof(Number))
+// console.log(stringNumber);
+// console.log(typeof(stringNumber))
+
+// ******************************* Operations **********************************
+
+// let value =3
+// let negValue =-value
+// //console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+
+//console.log(2%3)
+
+// let str1 ="hello"
+// let str2 =" Saugat"
+// let str3 =str1 + str2
+//console.log(str3);
+//console.log(3+4*5%3);
+
+//console.log(1+true)
+//console.log(+"");
+
+// let num1, num2, num3
+
+// num1=num2=num3=2+2
+//console.log([num1,num2,num3]);
+
+// let gameCounter =100
+// gameCounter++;
+//console.log(gameCounter)
+
+
+let x = 5;
+let y = ++x; // y will be 6, and x will be 6
+
+let x = 5;
+let y = x++; // y will be 5, and x will be 6
