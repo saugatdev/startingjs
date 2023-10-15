4041ea42747e9ce823a8916ed80b53f52571cc8d
Saugat Ghimire <68614843+Saugatify@users.noreply.github.com>
2023-10-15 05:31:44 +0000

    datatypes

---
+++ 01_basics/02datatype.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Saugat")
+
+let name ="hitesh"
+let age =18
+let isLoggedIn =false
+let state
+
+
+// primitive data type
+// number => 2 to power 52
+// big int
+// string =>""
+// boolean => true/false
+// null => stand alone value
+// undefined => value not defined
+// symbol => unique
+
+
+//object
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
