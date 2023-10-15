9be2634179fcfe479f4942e7c65291093314e606
Saugat Ghimire <68614843+Saugatify@users.noreply.github.com>
2023-10-15 12:22:21 +0000

    datatype conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,23 @@
+let score =null
+console.log(typeof score)
+console.log(typeof(score))
+
+//let valueInNumber = Number(score)
+
+
+
+//"33" => 33
+//"33abc" => NaN
+//true -> 1; false - 0
+
+let isLoggedIn =1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+
+let Number =33
+let stringNumber =String(Number);
+console.log(typeof(Number))
+console.log(stringNumber);
+console.log(typeof(stringNumber))
