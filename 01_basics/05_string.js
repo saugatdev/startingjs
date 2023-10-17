9802ac7981002bc672d2b1e8f52fca7ce0ff1e4a
Saugat Ghimire <68614843+Saugatify@users.noreply.github.com>
2023-10-17 15:39:42 +0000

    math and num

---
+++ 01_basics/05_string.js
@@ -0,0 +1,22 @@
+// const name ="saugat"
+// const repoCount=50
+
+// console.log(name+ repoCount+ "Value")
+
+// console.log(`Hello my name is ${name}`);
+
+const gameName =new String(`Saugat Ghimire`)
+
+// console.log(gameName[0]);
+
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toLocaleLowerCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString)
+
+const anotherString = gameName.slice(-17,0)
+console.log(anotherString)
