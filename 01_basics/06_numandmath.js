d521454375255958008e3e635120adc9dd4388d4
Saugat Ghimire <68614843+Saugatify@users.noreply.github.com>
2023-10-31 06:46:17 +0000

    date and time

--- 01_basics/06_numandmath.js
+++ 01_basics/06_numandmath.js
@@ -2,11 +2,11 @@
 
 // console.log(score);
 
-// const balance = new Number (100)
+const balance = new Number (100)
 // console.log(balance)
 
 // console.log(balance.toString().length);
-// console.log(balance.toFixed(2));
+console.log(balance.toFixed(2));
 
 
 // const otherNumber =123.8966
@@ -24,14 +24,14 @@
 // console.log(Math.ceil(4.1222222))
 // console.log(Math.floor(4.1222222))
 
-console.log(Math.random())
+// console.log(Math.random())
 
-console.log(Math.floor(Math.random()*10)+1)
+// console.log(Math.floor(Math.random()*10)+1)
 
-const min = 10
-const max = 20
+// const min = 10
+// const max = 20
 
-console.log(Math.floor(Math.random()*(max-min+1)+min))
+// console.log(Math.floor(Math.random()*(max-min+1)+min))
 
 
 
---
+++ 01_basics/07_dateandtime.js
@@ -0,0 +1,28 @@
+// let myDate =new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleDateString())
+// console.log(typeof myDate)
+
+// let myCreatedDate = new Date(2023,11,23)
+// console.log(myCreatedDate.toString());
+
+// let myCreatedDate = new Date("2023-01-14")
+
+// console.log(myCreatedDate.toString())
+// console.log(myCreatedDate.toISOString())
+// console.log(myCreatedDate.toLocaleString())
+// console.log(myCreatedDate.toLocaleDateString())
+
+//timestamp
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getDay())
