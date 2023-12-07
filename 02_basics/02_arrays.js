31212e313dd022fd2214d6a6bb09cf85b2c291bb
Saugat Ghimire <68614843+Saugatify@users.noreply.github.com>
2023-12-07 02:53:48 +0000

    Array completed

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,6 +1,26 @@
-const marvelheros =["thor","IronMan","SpiderMan"]
-const DC=["superman","flash","Batman"]
+// const marvelheros =["thor","IronMan","SpiderMan"]
+// const DC=["superman","flash","Batman"]
 
-const array3=marvelheros.concat(DC)
-console.log(array3)
+// const array3=marvelheros.concat(DC)
+// console.log(array3)
 
+// const all_new_heros=[...marvelheros,...DC]
+// console.log(all_new_heros)
+
+
+// const array=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+// const real_array = array.flat(Infinity)
+// console.log(real_array)
+
+
+//Data Scarping
+
+console.log(Array.isArray("Saugat"))
+console.log(Array.from("Saugat"))
+console.log(Array.from({name:"Saugat"})) //Interesting case for array
+
+let score1=100
+let score2=200
+let score3=400
+console.log(Array.of(score1,score2,score3));
