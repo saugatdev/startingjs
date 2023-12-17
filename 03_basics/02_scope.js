0186d892cf0c452a1fb56c63f382718635762920
Saugat Ghimire <68614843+Saugatify@users.noreply.github.com>
2023-12-17 22:17:55 +0545

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,19 @@
+
+let a =22;
+const b=222
+if (true){
+    let a =10
+    const b =11
+    var c= 12
+    var c= 12
+    // console.log(a)
+    // console.log(b);
+    console.log(c);
+
+
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
