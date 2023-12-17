a3107344e592cb451205459cbfe0f8c0265d633c
Saugat Ghimire <68614843+Saugatify@users.noreply.github.com>
2023-12-17 21:34:41 +0545

    functions

---
+++ 03_basics/01_function.js
@@ -0,0 +1,41 @@
+//function with many other
+function sayMyname(){
+    console.log("h");
+    console.log("i");
+    console.log("t");
+}
+//sayMyname()
+
+
+// function add(num1,num2){
+//     console.log(num1+num2)  
+//    }
+//   add(3,null)
+ 
+   
+   //Num1 and num2 are parameters
+  //And 3, null are arguments
+
+  function add(num1,num2){
+    // let result =num1 + num2
+    // return result
+    return num1+num2
+
+   }
+   
+   const result = add(3,5)
+   // console.log(result)
+
+   function loginUserMessage(username){
+    return`${username} just logged in`
+
+   }
+   console.log(loginUserMessage("Saugat"))
+
+
+
+ 
+   
+
+
+
