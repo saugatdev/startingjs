1b33cf67b1bd4a814c17dfcaca62d239a546a0ba
Saugat Ghimire <68614843+Saugatify@users.noreply.github.com>
2023-12-17 22:54:40 +0545

    functions arrow and this

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,41 @@
+
+const user ={
+    username:"saugat",
+    price:999,
+
+    welcomeMessage :  function(){
+        console.log(`${this.username} welcome to website`)
+        // console.log(this)
+    }
+}
+
+// user.welcomeMessage()
+// user.username="sam"
+// user.welcomeMessage()
+
+// console.log(this)
+
+// function chai(){
+//    let username="saugat"
+//     console.log(this.username)
+// }
+// chai()
+
+
+const chai =()=>{
+    let username="saugat"
+    console.log(this)
+}
+chai()
+
+
+///Arrow function
+//  const addtwp =(num1,num2)=>{
+//     return num1+num2
+//  }
+//  console.log(addtwo(3,4))
+
+ const addtwp =(num1,num2)=>{
+    return num1+num2
+ }
+ console.log(addtwo(3,4))
