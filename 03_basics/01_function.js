8fa11aaa3be9242c9bfdb257e4ef70a3dd44271f
Saugat Ghimire <68614843+Saugatify@users.noreply.github.com>
2023-12-17 22:03:53 +0545

    function1 done

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -26,16 +26,51 @@ function sayMyname(){
    const result = add(3,5)
    // console.log(result)
 
-   function loginUserMessage(username){
+//    function loginUserMessage(username){
+//     return`${username} just logged in`
+
+//    }
+//    console.log(loginUserMessage("Saugat"))
+
+
+   function loginUserMessage(username="Sam"){
+    if(!username){
+        console.log("Please enter the username")
+        return
+
+    }
     return`${username} just logged in`
 
    }
-   console.log(loginUserMessage("Saugat"))
+  // console.log(loginUserMessage())
 
 
+
+   //Shopping cart situations
+
+   function calculateCartPrice(val1,val2, ...num1){
+    return num1
+   }
+   //console.log(calculateCartPrice(100,400,300,200)) 
+
+
+    const user={
+        username:"saugat",
+        price:199
+
+    }
+    const user2={
+        username:"sak",
+        price:199
+
+    }
+
+    function handleObject(anyObject){
+        console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+        
+    }
+
+    // handleObject(user)
+    //  handleObject(user2)
 
  
-   
-
-
-
