fab25518fc1c514d8fefa1bea809a9322e747fce
Saugat Ghimire <68614843+Saugatify@users.noreply.github.com>
2023-12-19 23:07:56 +0545

    loop do while

---
+++ 05_iteraions.js/three.js
@@ -0,0 +1,9 @@
+//for of
+
+
+
+const arr =[1,2,3,4,6,7]
+
+for (const val of object) {
+    
+}
---
+++ 05_iteraions.js/two.js
@@ -0,0 +1,31 @@
+//do while loop
+
+
+let index =0;
+
+while (index<=10) {
+    //console.log(`Value of index is ${index}`);
+     index = index +2
+
+    
+}
+
+let myArray =['flash','batman','superman']
+
+let arr =0
+while (arr<myArray.length) {
+   //// console.log(`Superhero  is ${myArray[arr]}`);
+    arr =arr+1;
+}
+
+//do while loop 
+//interesting
+//udar wala type system
+// first take then check
+
+let score =11
+
+do {
+   console.log(`Score is ${score}`) 
+   score++
+} while (score <=10);
