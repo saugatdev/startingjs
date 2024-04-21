d8d31ec75ff20230a767eb19d530760d6cd48d09
Saugat <saugatg189@gmail.com>
2024-04-21 21:51:05 +0545

    Modules in JS and Console

---
+++ P_27_DayModulesinJS/main.js
@@ -0,0 +1,5 @@
+import {value,add,student as Studentobj} from "./script.js";
+
+console.log(value);
+console.log(add(2,3));
+console.log(Studentobj.age);
---
+++ P_27_DayModulesinJS/script.js
@@ -0,0 +1,12 @@
+const value = "Hello, world!";
+function add(a,b){
+    return a+b;
+}
+
+let student={
+    name:"Mahesh",
+    age:20,
+
+
+}
+export {value,add,student};
