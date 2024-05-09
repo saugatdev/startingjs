b3d0e3bcf8526db1532359654685de7c62c8cbf0
Saugat <saugatg189@gmail.com>
2024-05-09 22:49:49 +0545

    CONNECT TO ATLAS

---
+++ 002_Backend/004_Connection/src/db/index.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async ()=>{
+
+    try {
+        
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+    console.log( `MONGODB Connected!!! ${connectionInstance.connection.host}` );
+    console.log("Hey Saugat This is the only connection you can have !");
+
+    } catch (error) {
+        console.log("MONGODB Connection FAILED", error);
+        process.exit(1)
+        
+    }
+
+}
+
+export default connectDB
