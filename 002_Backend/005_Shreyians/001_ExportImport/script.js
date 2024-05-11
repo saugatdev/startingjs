6b3bc829896e9cdfd70c6d29242797c9216f75b6
Saugat <saugatg189@gmail.com>
2024-05-11 21:19:00 +0545

    middleware learned

--- 002_Backend/005_Shreyians/001_ExportImport/script.js
+++ 002_Backend/005_Shreyians/001_ExportImport/script.js
@@ -16,15 +16,40 @@
 // console.log(getRandomJoke)
 
 
-var figlet = require("figlet");
+// var figlet = require("figlet");
 
-figlet("Sheryians", function (err, data) {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
-  }
-  console.log(data);
+// figlet("SAUGAT", function (err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+
+
+
+const express = require('express');
+const { nextTick } = require('process');
+const app = express();
+
+
+app.use(function(req,res,next){
+  console.log("Hello");
+  next();
+})
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next(); 
 });
 
 
+
+
+app.get('/profile', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
